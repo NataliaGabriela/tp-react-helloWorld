52369eabb68176f6669bbc1965e4cef8c0cc95e5
Natalia Lopez <natigab.24@gmail.com>
2024-01-27 21:05:56 -0300

    modifico el componente helloWorld para enviar mediante props el valor my friend

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import HelloWorld from './componentes/helloWorld';
+
+const App = () => {
+    return (
+       <HelloWorld saludo = 'my friend!'></HelloWorld>
+    );
+};
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import HelloWorld from './componentes/helloWorld.jsx'
+import App from './App.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HelloWorld />
+    <App />
   </React.StrictMode>,
 )
