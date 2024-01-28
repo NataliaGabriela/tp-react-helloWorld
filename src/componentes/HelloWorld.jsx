f34b4dba0199437758270c4daaddde351301b75c
Natalia Lopez <natigab.24@gmail.com>
2024-01-27 22:14:39 -0300

    instalo la libreria bootstrap

--- src/componentes/HelloWorld.jsx
+++ src/componentes/HelloWorld.jsx
@@ -1,8 +1,14 @@
-import '../helloWorld.css'
+import Button from 'react-bootstrap/Button';
+
+import { useState } from "react";
 const HelloWorld = (props) => {
-    return (
-     <h1 className='tituloHelloWorld'>HELLO {props.saludo}</h1>
-    );
+  const [mensaje, setMensaje] = useState("");
+  return (
+    <section>
+      <h1 className="tituloHelloWorld">HELLO {props.saludo}</h1>
+      <Button variant="info" className="mx-2"></Button>
+    </section>
+  );
 };
 
 export default HelloWorld;
