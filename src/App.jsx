921439ee8c2df876dfae4e3b546b12e7e178f037
Natalia Lopez <natigab.24@gmail.com>
2024-01-29 12:55:17 -0300

    corrijo nombre de componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./helloWorld.css";
 import React from "react";
-import HelloWorld from "./componentes/helloWorld";
+import HelloWorld from "./componentes/HelloWorld";
+
 
 const App = () => {
   return <HelloWorld saludo="my friend"></HelloWorld>;
